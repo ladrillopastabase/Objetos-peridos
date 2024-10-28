4db01db1d1033246d53c68a2603847e0a8533220
ladrillopastabase <167151754+ladrillopastabase@users.noreply.github.com>
2024-10-28 13:44:55 -0300

    correccion

--- home.py
+++ home.py
@@ -11,16 +11,13 @@ import requests
 import base64
 from supabase import create_client, Client
 
-#from dotenv import load_dotenv
-#load_dotenv("C:/Users/ferna/OneDrive/Escritorio/Ferna/Programación/Pagina Objetos perdidos/variables.env")
-#imagebbkey: str = os.environ.get("IMAGEBB_KEY")
-imagebbkey: str = st.secrets["IMAGEBB_KEY"]
+from dotenv import load_dotenv
+load_dotenv("C:/Users/ferna/OneDrive/Escritorio/Ferna/Programación/Pagina Objetos perdidos/variables.env")
+imagebbkey: str = os.environ.get("IMAGEBB_KEY")
 print("Pagina inicializada correctamente!")
 
-#url: str = os.environ.get("SUPABASE_URL")
-#key: str = os.environ.get("SUPABASE_KEY")
-url: str = st.secrets["SUPABASE_URL"]
-key: str = st.secrets["SUPABASE_KEY"]
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 print("Conexión base de datos realizada correctamente!")
 
@@ -47,21 +44,21 @@ result_df = pd.concat([df[['id']], item_df], axis=1) # Concatenar el DataFrame o
 
 print(result_df)
 
-# Dividir el DataFrame en grupos de 3 elementos
-num_columns = 3
-num_rows = len(result_df)
+edited_df = result_df[["item_category"]].copy()
 
-# Dividir el DataFrame en grupos de 3 elementos, comenzando desde el último
-for start in range(len(result_df), 0, -num_columns):
-    cols = st.columns(num_columns)
-    for i, col in enumerate(cols):
-        index = start - i - 1
-        if index >= 0:
-            row = result_df.iloc[index]
-            with col:
-                container = st.container(border=True)
-                container.image(row['item_image_url'])
-                container.markdown(f'''{row['item_name']}  
-                                   :red[Estado:] {row['item_status']}''')
-                container.caption(f"Descripción: {row['item_description']}")
-                container.caption(f"Fecha: {row['item_in']}")
+print(edited_df)
+
+st.dataframe(result_df, use_container_width=True, hide_index=True)
+
+
+st.header("Objetos en existencia")
+priority_plot = (
+    alt.Chart(edited_df)
+    .mark_arc()
+    .encode(theta="count():Q", color="item_category:N")
+    .properties(height=300)
+    .configure_legend(
+        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
+    )
+)
+st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
